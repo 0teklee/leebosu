95b62ca5dcd18e185bdc1da09690bdb0808f1dd6
0teklee <leetekwoo@gmail.com>
2025-04-11 16:50:43 +0900

    style: update global styles, animations and theme utilities

--- src/utils/icon-paths.ts
+++ src/utils/icon-paths.ts
@@ -4,3 +4,10 @@ export const [StackIconPath, XIconPath, MoonIconPath, SunIconPath] = [
 	"M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z",
 	"M12 4V2M12 22v-2M4.22 4.22l1.42 1.42M18.36 18.36l1.42 1.42M2 12h2M20 12h2M4.22 19.78l1.42-1.42M18.36 5.64l1.42-1.42M12 6a6 6 0 100 12 6 6 0 000-12z",
 ];
+
+export const [
+	ChevronDownIconPath,
+	ChevronUpIconPath,
+	ChevronLeftIconPath,
+	ChevronRightIconPath,
+] = ["M6 9l6 6 6-6", "M6 15l6-6 6 6", "M6 9l6 6 6-6", "M6 15l6-6 6 6"];
