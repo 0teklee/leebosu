8f8b12d41fccea406e67fa1585bfbbb3b75146bb
0teklee <leetekwoo@gmail.com>
2025-04-21 20:01:00 +0900

    chore(booking): add detailed comments to deprecated price

--- src/domains/booking/utils.ts
+++ src/domains/booking/utils.ts
@@ -21,19 +21,23 @@ export function getCurrentKey() {
 	return FORM_INPUT_KEY_MAP[currentStep];
 }
 
-// export function calculateEstimatedPrice(
-// 	mainCategory: string | null,
-// 	subCategory: string | null
-// ): number {
-// 	const fallback = { subCategories: [], baseMinPrice: 0 };
-
-// 	const category = mainCategory
-// 		? SERVICES[mainCategory as keyof typeof SERVICES]
-// 		: fallback;
-
-// 	const subCategoryPrice = subCategory
-// 			? category.subCategories.find((sub) => sub.id === subCategory)?.basePrice
-// 		: undefined;
-
-// 	return subCategoryPrice ?? category.baseMinPrice;
-// }
+/**
+ * @desc 예약 가격 계산 함수
+ * @param mainCategory - 메인 카테고리
+ * @param subCategory - 서브 카테고리
+ * @returns 예약 가격
+ * @todo 예상 가격 주석처리 (서비스 정책 변경) 
+	 export function calculateEstimatedPrice(
+	 	mainCategory: string | null,
+	 	subCategory: string | null
+	 ): number {
+	 	const fallback = { subCategories: [], baseMinPrice: 0 };	
+	 	const category = mainCategory
+	 		? SERVICES[mainCategory as keyof typeof SERVICES]
+	 		: fallback;	
+	 	const subCategoryPrice = subCategory
+	 			? category.subCategories.find((sub) => sub.id === subCategory)?.basePrice
+		: undefined;
+	return subCategoryPrice ?? category.baseMinPrice;
+}
+*/
