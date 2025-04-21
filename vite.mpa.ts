c907ae68eecbe9b74817c38409cece9df131cd86
0teklee <leetekwoo@gmail.com>
2025-04-21 23:57:02 +0900

    refactor: applied vite.meta vars

--- vite.mpa.ts
+++ vite.mpa.ts
@@ -1,43 +1,38 @@
 import { Pages } from "vite-plugin-mpa-plus";
+import {
+	AboutMetadata,
+	ErrorMetadata,
+	MainMetadata,
+	NotFoundMetadata,
+} from "./vite.meta";
+
 export const mpaPlusConfig: Pages = {
 	index: {
 		filename: "index.html",
 		template: "public/index.html",
 		inject: {
-			data: {
-				title: "LEEBOSU - 안양 집수리",
-				description: "안양 지역 집수리 전문 업체, 이보수입니다.",
-			},
+			data: MainMetadata,
 		},
 	},
 	about: {
 		filename: "about.html",
 		template: "public/index.html",
 		inject: {
-			data: {
-				title: "LEEBOSU - 안양 지역 집수리",
-				description: "안양 지역 집수리 전문 업체, 이보수입니다.",
-			},
+			data: AboutMetadata,
 		},
 	},
 	notFound: {
 		filename: "404.html",
 		template: "public/index.html",
 		inject: {
-			data: {
-				title: "LEEBOSU",
-				description: "페이지를 찾을 수 없습니다.",
-			},
+			data: NotFoundMetadata,
 		},
 	},
 	error: {
 		filename: "error.html",
 		template: "public/index.html",
 		inject: {
-			data: {
-				title: "LEEBOSU",
-				description: "오류가 발생했습니다.",
-			},
+			data: ErrorMetadata,
 		},
 	},
 };
