8d21d9a6c96210e68217e954403bca819b254e14
0teklee <leetekwoo@gmail.com>
2025-05-05 15:21:17 +0900

    chore: fixed business items + add ETC

--- src/business.ts
+++ src/business.ts
@@ -5,11 +5,11 @@ export const SERVICES = {
 		subCategories: [
 			"세면대 수리/교체",
 			"수도꼭지 수리/교체",
-			"변기 수리/교체",
+			"변기 수리/교체 (막힘 포함)",
 			"타일 수리/교체",
 			"샤워기 수리/교체",
-			"변기 막힘",
 			"하수구 막힘",
+			"기타",
 		],
 		material: [
 			["대림", 80000],
@@ -19,25 +19,19 @@ export const SERVICES = {
 	},
 	electric: {
 		name: "전기",
-		subCategories: ["조명 수리/교체", "콘센트 수리/교체"],
+		subCategories: ["조명 수리/교체", "콘센트 수리/교체", "기타"],
 		material: [],
 		custom: true,
 	},
 	boiler: {
 		name: "난방/누수",
-		subCategories: ["바닥 난방/누수 수리 ", "상하수도 누수 수리"],
+		subCategories: ["바닥 난방/누수 수리 ", "상하수도 누수 수리", "기타"],
 		material: [],
 		custom: false,
 	},
 	repair: {
 		name: "집수리",
-		subCategories: [
-			"문 수리/교체",
-			"창문 수리/교체",
-			"도배",
-			"페인트",
-			"바닥재 시공",
-		],
+		subCategories: ["문 수리/교체", "창문 수리/교체", "바닥재 시공", "기타"],
 		material: [
 			["브랜드와 제품 종류에 따라 재료 가격이 다를 수 있습니다.", null],
 		],
