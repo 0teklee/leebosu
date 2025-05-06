7a6d501da0f55f8fa783339157f52a6c9375e147
0teklee <leetekwoo@gmail.com>
2025-05-06 18:24:13 +0900

    fix(atom): details overflow y + height

--- src/components/atom/Details.tsx
+++ src/components/atom/Details.tsx
@@ -6,9 +6,15 @@ import { useState } from "react";
 export default function Details({
 	title,
 	children,
+	detailsClassName,
+	summaryClassName,
+	height = "h-30",
 }: {
 	title: string;
 	children: React.ReactNode;
+	height?: `h-${number | string}`;
+	detailsClassName?: string;
+	summaryClassName?: string;
 }) {
 	const [isOpen, setIsOpen] = useState(false);
 	const [isExitAnimate, triggerAnim, animDuration] = useAnimateDelay(200);
@@ -22,16 +28,25 @@ export default function Details({
 					setIsOpen((prev) => !prev);
 				});
 			}}
-			className="group border-b border-theme/20 pb-4"
+			className={clsx(
+				"group after:content-[''] after:block after:h-0 after:border-b after:border-theme/20 after:pb-4",
+				height,
+				detailsClassName
+			)}
 		>
-			<summary className="flex justify-between items-center cursor-pointer list-none py-2 text-xl sm:text-2xl font-semibold text-theme transition-colors">
+			<summary
+				className={clsx(
+					"flex justify-between items-center cursor-pointer list-none py-2 text-xl sm:text-2xl font-semibold text-theme transition-colors",
+					summaryClassName
+				)}
+			>
 				{title}
 				<ChevronIcon className="w-5 h-5 transition-transform duration-300 group-open:rotate-180" />
 			</summary>
 			{isOpen && (
 				<div
 					className={clsx(
-						"h-40 overflow-y-auto",
+						"overflow-y-auto",
 						"pt-4 px-2 sm:px-4",
 						"origin-top anim-direction-alternate anim-timing-ease-in-out",
 						animDuration,
