c9b6ff33993ca74613f0bbc0e72441c033b69f2a
0teklee <leetekwoo@gmail.com>
2025-05-06 14:37:22 +0900

    fix(book): add preview text description

--- src/domains/book/components/Preview.tsx
+++ src/domains/book/components/Preview.tsx
@@ -6,7 +6,8 @@ interface PreviewDisplayProps {
 }
 
 export default function BookingPreview({ formState }: PreviewDisplayProps) {
-	const { mainCategory, subCategory, date, location, contact } = formState;
+	const { mainCategory, subCategory, date, location, contact, description } =
+		formState;
 
 	const mainService = mainCategory ? SERVICES[mainCategory] : null;
 	const isCustomService = mainService?.custom;
@@ -19,17 +20,18 @@ export default function BookingPreview({ formState }: PreviewDisplayProps) {
 					<p>원하시는 재료를 미리 준비해주시면 공임비만 받습니다.</p>
 				</div>
 			)}
-			<div className="space-y-1 *:text-xs text-secondary">
-				<p className="mb-2 text-theme font-semibold">선택하신 예약 정보</p>
-				{mainService && <p>대분류 : {mainService.name}</p>}
-				{subCategory && <p>소분류 : {subCategory}</p>}
-				{date && <p>날짜 : {new Date(date as string).toLocaleDateString()}</p>}
-				{location && <p>지역 : {location}</p>}
-				{contact && <p>연락처 : {contact}</p>}
-			</div>
 			<p className="mt-1 text-xs text-destructive/80">
 				* 실제 비용은 현장 상황에 따라 달라질 수 있습니다.
 			</p>
+			<div className="space-y-1 *:text-xs text-secondary">
+				<p className="mb-2 text-theme font-semibold">선택하신 예약 정보</p>
+				{mainService && <p>서비스 : {mainService.name}</p>}
+				{subCategory && <p>상세 : {subCategory}</p>}
+				{date && <p>날짜 : {new Date(date as string).toLocaleDateString()}</p>}
+				{location && <p>지역 : {location}</p>}
+				{contact && <p>연락처 : {contact}</p>}
+				{description && <p>부가 사항 : {description}</p>}
+			</div>
 		</div>
 	);
 }
